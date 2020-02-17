c389387bf77216593bccb7281463437cdf22ac95
Will Corcoran <corcoranwill@gmail.com>
2020-02-17 02:28:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(calls)
   calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese(cheese))
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese.detect do |food|
     food == "cheddar" || food == "gouda" || food == "camembert"
